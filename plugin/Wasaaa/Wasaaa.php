e95c980df12e517b2b56c474a8eb61c1cc23058c
Nazim Boudeffa <boudeffa@gmail.com>
2019-10-29 19:07:07 +0100

    Fixing Wasaaa plugin

---
+++ plugin/Wasaaa/Wasaaa.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
+
+class Wasabi extends PluginAbstract {
+
+    public function getDescription() {
+        return "Manage Wasabi Videos Informations";
+    }
+
+    public function getName() {
+        return "Wasaaa";
+    }
+
+    public function getUUID() {
+        return "0fdbbf98-fa4f-11e9-8f0b-362b9e155667";
+    }
+
+    public function getPluginVersion() {
+        return "1.0";
+    }
+
+    public function getTags() {
+        return array('free', 'wasabi', 'video player');
+    }
+
+    public function getEmptyDataObject() {
+        return "";
+    }
+
+    public function getHeadCode() {
+        return "";
+    }
+}
